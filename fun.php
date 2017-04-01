b0815ea822b16f11448d6627112fb75e0847d844
rajesh0608 <rajeshy0608@gmail.com>
2017-04-01 22:28:20 +0530

    Function added

---
+++ fun.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: abc
+ * Date: 3/7/2017
+ * Time: 12:57 AM
+ *//*
+function abc(){
+    echo 'Call function';
+}
+abc();
+abc();
+abc();*/
+
+// Pass parameter to function
+
+function abc($name){
+    echo 'Hello '. $name .' to know the function';
+}
+abc('Antas');
+
+
+
+
+<form>
+
+
+</form>
